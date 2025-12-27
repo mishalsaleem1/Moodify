9e693d4e8299f8293fff57b6c941655d59c90633
mishalsaleem1 <mishalsaleem88891@gmail.com>
2025-12-28 01:05:12 +0500

    update

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -152,7 +152,7 @@ const Login = () => {
                   value={formData.email}
                   onChange={handleChange}
                   required
-                  className="glass-input w-full pl-12 focus:ring-2 focus:ring-cosmic-purple text-white dark:text-white"
+                  className="glass-input w-full pl-12 focus:ring-2 focus:ring-cosmic-purple text-gray-100 dark:text-gray-100"
                 />
               </div>
             </div>
@@ -168,7 +168,7 @@ const Login = () => {
                   value={formData.password}
                   onChange={handleChange}
                   required
-                  className="glass-input w-full pl-12 pr-12 focus:ring-2 focus:ring-cosmic-purple text-white dark:text-white"
+                  className="glass-input w-full pl-12 pr-12 focus:ring-2 focus:ring-cosmic-purple text-gray-100 dark:text-gray-100"
                 />
                 <button
                   type="button"
